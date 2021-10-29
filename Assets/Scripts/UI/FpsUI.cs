4a8d42cfe5287b3bfee5fcb3f584c979abc1bdec
Ezioleq <31481375+Ezioleq@users.noreply.github.com>
2021-10-30 01:04:37 +0200

    Saving FPS Counter show state

--- Assets/Scripts/UI/FpsUI.cs
+++ Assets/Scripts/UI/FpsUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using TMPro;
 
@@ -7,12 +8,45 @@ public class FpsUI : MonoBehaviour {
 	[SerializeField] private float _fpsRefreshRate = 1f;
 	private float _fpsCounter;
 	private float _timer;
+
+	[SerializeField] private KeyCode _toggleKey = KeyCode.F3;
+	private string _showFpsStateKey = "show_fps";
  
+	private void Awake() {
+		// Allow only one instance by destroying another ones
+		DontDestroyOnLoad(this.gameObject);
+		if (FindObjectsOfType(GetType()).Length > 1)
+			Destroy(this.gameObject);
+		
+		LoadState();
+	}
+
 	private void Update() {
+		if (Input.GetKeyDown(_toggleKey))
+			ToggleFPS();
+
 		if (Time.time > _timer) {
 			_fpsCounter = (int)(1f / Time.unscaledDeltaTime);
 			_fpsCounterText.text = _fpsCounter.ToString() + " FPS";
 			_timer += _fpsRefreshRate;
 		}
+	}
+
+	private void ToggleFPS() {
+		SaveState();
+		LoadState();
+	}
+
+	private void SaveState() {
+		PlayerPrefs.SetInt(
+			_showFpsStateKey,
+			Convert.ToInt32(!_fpsCounterText.gameObject.activeSelf)
+		);
+	}
+
+	private void LoadState() {
+		_fpsCounterText.gameObject.SetActive(
+			Convert.ToBoolean(PlayerPrefs.GetInt(_showFpsStateKey, 0))
+		);
 	}
 }
